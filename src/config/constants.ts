1788384d3319b3f767b92cdffaf37bcbf6f45add
Victor Villavicencio <victorv@titamedia.com>
2020-11-13 00:37:43 -0500

    add constants file, kuepa theming, and BEM

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+export class Constants {
+  public static SERVICES_URL = {
+    opendotaUrl: "https://api.opendota.com/api",
+    proplayers: "/proPlayers",
+    players: "/players",
+    heros: "/heroes"
+  };
+}
